aa01715ed34ba34e55a2c4066665d9786f3970b7
Khometa <pavel.khometa@emc.com>
2016-06-12 20:07:04 +0300

    add .equals.

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appManager/ApplicationManager.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -36,9 +37,9 @@ public class ApplicationManager {
             // groupHelper.wd = new FirefoxDriver();
             // contactHelper.wd = new FirefoxDriver();
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.CHROME) {
+        } else if (Objects.equals(browser, BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (Objects.equals(browser, BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
         // init attribute of object = WD
---
+++ sandbox/src/main/java/Equality.java
@@ -0,0 +1,5 @@
+/**
+ * Created by khomep on 12-Jun-16.
+ */
+public class Equality {
+}
