5253d1746de97d6c8ec406ff3471d8ba4cb536c4
Khometa <pavel.khometa@emc.com>
2016-08-03 11:47:36 +0300

    add Selenium driver

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appManager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.appManager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -18,16 +22,16 @@ import java.util.concurrent.TimeUnit;
  * Created by khomep on 08-Jun-16.
  */
 public class ApplicationManager {
-    //FirefoxDriver wd;
-    //private final Properties properties;
-    //private static Properties properties;
+        //FirefoxDriver wd;
+        //private final Properties properties;
+        //private static Properties properties;
     public static Properties properties;
-    //private Properties properties;
-    //public Properties properties;
+        //private Properties properties;
+        //public Properties properties;
     WebDriver wd;
     private ContactHelper contactHelper;
-    // private final ContactHelper contactHelper = new ContactHelper();
-    // private final GroupHelper groupHelper = new GroupHelper();
+        // private final ContactHelper contactHelper = new ContactHelper();
+        // private final GroupHelper groupHelper = new GroupHelper();
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
@@ -48,16 +52,26 @@ public class ApplicationManager {
             //this is constructor ??? // lesson 7.4
         dbHelper = new DbHelper();
 
-        if (browser.equals(BrowserType.FIREFOX)) {
+        if ("".equals((properties.getProperty("selenium.server")))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
                 // .....goes method 'setup'
                 // groupHelper.wd = new FirefoxDriver();
                 // contactHelper.wd = new FirefoxDriver();
-            wd = new FirefoxDriver();
-        } else if (Objects.equals(browser, BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (Objects.equals(browser, BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
+                wd = new FirefoxDriver();
+            } else if (Objects.equals(browser, BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (Objects.equals(browser, BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            } else{
+                // using remote servet
+                DesiredCapabilities capabilities = new DesiredCapabilities;
+                capabilities.setBrowserName(browser);
+                wd = new RemoteWebDriver(new URL
+                        (properties.getProperty("selenium.server")),capabilities);
+            }
         }
+
+
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
